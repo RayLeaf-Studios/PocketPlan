ed0811e39487fde195c8125abdfb8609e005294e
estep <e.stepanov611@gmail.com>
2020-04-03 06:14:26 +0200

    fixed problem with secondary constructor in birthday class

--- app/src/main/java/com/example/j7_003/logic/Birthday.kt
+++ app/src/main/java/com/example/j7_003/logic/Birthday.kt
@@ -1,8 +1,5 @@
 package com.example.j7_003.logic
 
-data class Birthday constructor(var name: String, var month: Int, var day: Int) {
-    lateinit var note: String
-    constructor(name: String, month: Int, day: Int, note: String) : this(name, month, day) {
-        this.note = note
-    }
+data class Birthday constructor(var name: String, var month: Int, var day: Int, var note: String) {
+    constructor(name: String, month: Int, day: Int) : this(name, month, day, "")
 }
