d360c0b05873d74b662ff9eeea1e4fa2ef338d77
estep <e.stepanov611@gmail.com>
2020-04-04 03:29:00 +0200

    added function to create notification <- needs to be specified further and can be called from MainActivity

--- app/src/main/java/com/example/j7_003/MainActivity.kt
+++ app/src/main/java/com/example/j7_003/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.example.j7_003
 
 import android.annotation.SuppressLint
+import android.app.*
+import android.content.Context
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import android.view.*
 import android.widget.Toast
@@ -67,6 +73,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
                     .commit()
             }
             R.id.todolist -> {
+                createNotification("new description")
                 supportActionBar?.title = "ToDo List"
                 todoFragment = TodoFragment()
                 supportFragmentManager
@@ -105,6 +112,39 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         }else {
             super.onBackPressed()
         }
+    }
+
+    private fun createNotification(description: String) {
+        lateinit var notificationChannel: NotificationChannel
+        lateinit var builder: Notification.Builder
+        val channelId = "com.example.j7_003"
+        val notificationManager: NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        val intent = Intent(this, LauncherActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            notificationChannel = NotificationChannel(channelId, description, NotificationManager.IMPORTANCE_DEFAULT)
+            notificationChannel.enableLights(true)
+            notificationChannel.lightColor = Color.GREEN
+            notificationChannel.enableVibration(true)
+            notificationManager.createNotificationChannel(notificationChannel)
+
+            builder = Notification.Builder(this, channelId)
+                .setContentTitle("new content title")
+                .setContentText("new content text")
+                .setSmallIcon(R.drawable.ic_action_todo)
+                .setLargeIcon(BitmapFactory.decodeResource(this.resources, R.mipmap.ic_launcher_round))
+                .setContentIntent(pendingIntent)
+        } else {
+            builder = Notification.Builder(this)
+                .setContentTitle("new content title")
+                .setContentText("new content text")
+                .setSmallIcon(R.drawable.ic_action_todo)
+                .setLargeIcon(BitmapFactory.decodeResource(this.resources, R.mipmap.ic_launcher_round))
+                .setContentIntent(pendingIntent)
+        }
+        notificationManager.notify(1234, builder.build())
     }
 }
 
