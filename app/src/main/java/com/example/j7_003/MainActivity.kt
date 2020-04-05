b6fc9eb418d97758a4ec046d3dde128b04783b57
Micha <gonderjake@gmail.com>
2020-04-05 13:35:10 +0200

    Changed menu from drawer to bottom nav Removed unnecessary classes / files Updated todo

--- app/src/main/java/com/example/j7_003/MainActivity.kt
+++ app/src/main/java/com/example/j7_003/MainActivity.kt
@@ -1,46 +1,94 @@
 package com.example.j7_003
 
-import android.annotation.SuppressLint
 import android.content.Context
 import android.os.Bundle
-import android.view.*
-import android.widget.Toast
-import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.GravityCompat
 import androidx.fragment.app.FragmentTransaction
 import com.example.j7_003.data.Database
 import com.example.j7_003.data.database_objects.Birthday
 import com.example.j7_003.fragments.*
 import com.example.j7_003.notifications.NotificationHandler
-import com.google.android.material.navigation.NavigationView
-import kotlinx.android.synthetic.main.app_bar_main.*
-import kotlinx.android.synthetic.main.fragment_home.*
-import kotlinx.android.synthetic.main.main_panel.*
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-@SuppressLint("Registered")
-class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+class MainActivity : AppCompatActivity(){
 
-    lateinit var homeFragment: HomeFragment
-    lateinit var calenderFragment: CalenderFragment
-    lateinit var birthdayFragment: BirthdayFragment
-    lateinit var settingsFragment: SettingsFragment
-    lateinit var todoFragment: TodoFragment
-    lateinit var notificationHandler: NotificationHandler
+    private lateinit var homeFragment: HomeFragment
+    private lateinit var calenderFragment: CalenderFragment
+    private lateinit var birthdayFragment: BirthdayFragment
+    private lateinit var settingsFragment: SettingsFragment
+    private lateinit var todoFragment: TodoFragment
+    private lateinit var notificationHandler: NotificationHandler
     lateinit var database: Database
 
-
-    @SuppressLint("InflateParams", "ResourceAsColor")
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_panel)
 
-        setSupportActionBar(toolBar)
-        val actionBar = supportActionBar
-        actionBar?.title = "Home"
         database = Database(this)
-        val drawerToggle: ActionBarDrawerToggle = object : ActionBarDrawerToggle(this, drawerLayout, toolBar, (R.string.open), (R.string.close)){}
+
+        val bottomNavigation : BottomNavigationView = findViewById(R.id.btm_nav)
+
+        bottomNavigation.setOnNavigationItemSelectedListener { item ->
+            when(item.itemId){
+                R.id.home -> {
+                    supportActionBar?.title = "Home"
+                    homeFragment = HomeFragment()
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.frame_layout, homeFragment)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit()
+                }
+                R.id.settings -> {
+                    supportActionBar?.title = "Settings"
+                    settingsFragment = SettingsFragment()
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.frame_layout, settingsFragment)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit()
+                }
+                R.id.todolist -> {
+                    notificationHandler.createDebugNotification()
+                    supportActionBar?.title = "ToDo List"
+                    todoFragment = TodoFragment()
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.frame_layout, todoFragment)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit()
+                }
+                R.id.birthdays -> {
+                    //debug >
+                    notificationHandler.notifyUpcomingBirthday(
+                        Birthday(
+                            "Eugen",
+                            12,
+                            24
+                        )
+                    )
+                    //debug <
+                    supportActionBar?.title = "Birthdays"
+                    birthdayFragment = BirthdayFragment()
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.frame_layout, birthdayFragment)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit()
+                }
+                R.id.calendar -> {
+                    supportActionBar?.title="Calendar"
+                    calenderFragment = CalenderFragment()
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.frame_layout, calenderFragment)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit()
+                }
+            }
+            true
+        }
 
         notificationHandler =
             NotificationHandler(
@@ -48,11 +96,9 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
                 getSystemService(Context.NOTIFICATION_SERVICE)
             )
 
-        drawerToggle.isDrawerIndicatorEnabled = true
-        drawerLayout.addDrawerListener(drawerToggle)
-        drawerToggle.syncState()
-
-        nav_view.setNavigationItemSelectedListener(this)
+        /**
+         * initializes homeFragment as first fragment
+         */
 
         homeFragment = HomeFragment()
         supportFragmentManager
@@ -60,76 +106,6 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
             .replace(R.id.frame_layout, homeFragment)
             .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
             .commit()
-    }
-
-    override fun onNavigationItemSelected(menuItem: MenuItem): Boolean {
-        when(menuItem.itemId){
-            R.id.home -> {
-                supportActionBar?.title = "Home"
-                homeFragment = HomeFragment()
-                supportFragmentManager
-                    .beginTransaction()
-                    .replace(R.id.frame_layout, homeFragment)
-                    .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
-                    .commit()
-            }
-            R.id.settings -> {
-                supportActionBar?.title = "Settings"
-                settingsFragment = SettingsFragment()
-                supportFragmentManager
-                    .beginTransaction()
-                    .replace(R.id.frame_layout, settingsFragment)
-                    .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
-                    .commit()
-            }
-            R.id.todolist -> {
-                notificationHandler.createDebugNotification()
-                supportActionBar?.title = "ToDo List"
-                todoFragment = TodoFragment()
-                supportFragmentManager
-                    .beginTransaction()
-                    .replace(R.id.frame_layout, todoFragment)
-                    .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
-                    .commit()
-            }
-            R.id.birthdays -> {
-                //debug >
-                notificationHandler.notifyUpcomingBirthday(
-                    Birthday(
-                        "Eugen",
-                        12,
-                        24
-                    )
-                )
-                //debug <
-                supportActionBar?.title = "Birthdays"
-                birthdayFragment = BirthdayFragment()
-                supportFragmentManager
-                    .beginTransaction()
-                    .replace(R.id.frame_layout, birthdayFragment)
-                    .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
-                    .commit()
-            }
-            R.id.calendar -> {
-                supportActionBar?.title = "Calendar"
-                calenderFragment = CalenderFragment()
-                supportFragmentManager
-                    .beginTransaction()
-                    .replace(R.id.frame_layout, calenderFragment)
-                    .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
-                    .commit()
-            }
-        }
-        drawerLayout.closeDrawer(GravityCompat.START)
-        return true
-    }
-
-    override fun onBackPressed() {
-        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
-            drawerLayout.closeDrawer(GravityCompat.START)
-        }else {
-            super.onBackPressed()
-        }
     }
 
 }
