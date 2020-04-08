0ab3fb215f1d9611834b089c3d686a455140b589
Eugen Stepanov <e.stepanov611@gmail.com>
2020-04-08 16:11:15 +0200

    changed hasReminder function for birthday objects

--- app/src/main/java/com/example/j7_003/data/database_objects/Birthday.kt
+++ app/src/main/java/com/example/j7_003/data/database_objects/Birthday.kt
@@ -3,5 +3,5 @@ package com.example.j7_003.data.database_objects
 data class Birthday constructor(var name: String, var month: Int, var day: Int, var daysToRemind: Int?) {
     constructor(name: String, month: Int, day: Int): this(name, month, day, null)
 
-    fun hasReminder(): Boolean = daysToRemind != null
+    fun hasReminder(): Boolean = daysToRemind != 0
 }
