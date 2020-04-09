da8f624a674ec129179520d113cc15a4d7be0684
Eugen Stepanov <e.stepanov611@gmail.com>
2020-04-09 16:29:13 +0200

    updated notification system, it's now fully implemented and will send notifications once per day if there are upcoming or current birthdays

--- app/src/main/java/com/example/j7_003/notifications/NotificationReceiver.kt
+++ app/src/main/java/com/example/j7_003/notifications/NotificationReceiver.kt
@@ -26,7 +26,6 @@ class NotificationReceiver : BroadcastReceiver() {
     private val calendar = Calendar.getInstance()
 
     override fun onReceive(context: Context?, intent: Intent?) {
-
         if (context != null) {
             myContext = context
         } else {
@@ -39,91 +38,107 @@ class NotificationReceiver : BroadcastReceiver() {
             return
         }
 
-        val notifiableBirthdays: ArrayList<Birthday> = getUpcomingBirthdays()
+        val notifiableUpcomingBirthdays = getUpcomingBirthdays()
+        val notifiableCurrentBirthdays = getCurrentBirthdays()
 
-        notifiableBirthdays.forEach { n ->
-            if (n.daysToRemind == 0) {
-                notifyBirthdayNow(n)
-            } else {
-                notifyUpcomingBirthday(n)
+        if (notifiableCurrentBirthdays.size > 1) {
+            notifyCurrentBirthdays(notifiableCurrentBirthdays.size)
+        } else if (notifiableCurrentBirthdays.size == 1) {
+            notifyBirthdayNow(notifiableCurrentBirthdays[0])
+        }
+
+        if (notifiableUpcomingBirthdays.size > 1) {
+            notifyUpcomingBirthdays(notifiableUpcomingBirthdays.size)
+        } else if (notifiableCurrentBirthdays.size == 1) {
+            notifyUpcomingBirthday(notifiableUpcomingBirthdays[0])
+        }
+
+        when {
+            notifiableCurrentBirthdays.size > 1 -> {
+                notifyCurrentBirthdays(notifiableCurrentBirthdays.size)
             }
+            notifiableCurrentBirthdays.size == 1 -> {
+                notifyBirthdayNow(notifiableCurrentBirthdays[0])
+            }
+            else -> return
         }
     }
 
     private fun getUpcomingBirthdays(): ArrayList<Birthday> {
         val upcomingBirthdays = ArrayList<Birthday>()
         database.birthdayList.forEach { n ->
-            if (n.month == calendar.get(Calendar.MONTH)+1 && (n.day == calendar.get(Calendar.DAY_OF_MONTH) || (n.day - n.daysToRemind) == calendar.get(Calendar.DAY_OF_MONTH))) {
+            if (n.month == calendar.get(Calendar.MONTH)+1 && (n.day - n.daysToRemind) == calendar.get(Calendar.DAY_OF_MONTH) &&  n.daysToRemind != 0) {
                 upcomingBirthdays.add(n)
             }
         }
         return upcomingBirthdays
     }
 
-    private fun notifyBirthdayNow(birthday: Birthday) {
-        val notificationManager = myContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        val repeatingIntent = Intent(myContext, MainActivity::class.java)
-
-        repeatingIntent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
-
-        val pendingIntent = PendingIntent.getActivity(myContext, 100, repeatingIntent, PendingIntent.FLAG_UPDATE_CURRENT)
-
-        val channelId = "Birthdays"
-        val builder: Notification.Builder
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val notificationChannel = NotificationChannel(channelId, "Birthday Notifications", NotificationManager.IMPORTANCE_HIGH)
-            notificationChannel.enableLights(true)
-            notificationChannel.lightColor = Color.GREEN
-            notificationChannel.enableVibration(true)
-            notificationManager.createNotificationChannel(notificationChannel)
-
-            builder = Notification.Builder(myContext, channelId)
-                .setContentTitle("Birthday")
-                .setContentText("It's ${birthday.name}s birthday!")
-                .setSmallIcon(R.drawable.ic_action_birthday)
-                .setLargeIcon(
-                    BitmapFactory.decodeResource(myContext.resources,
-                        R.drawable.ic_action_birthday
-                    ))
-                .setContentIntent(pendingIntent)
-                .setAutoCancel(true)
-        } else {
-            builder = Notification.Builder(myContext)
-                .setContentTitle("Birthday")
-                .setContentText("It's ${birthday.name}s birthday!")
-                .setSmallIcon(R.drawable.ic_action_birthday)
-                .setLargeIcon(
-                    BitmapFactory.decodeResource(myContext.resources,
-                        R.mipmap.ic_launcher_round
-                    ))
-                .setContentIntent(pendingIntent)
-                .setAutoCancel(true)
+    private fun getCurrentBirthdays(): ArrayList<Birthday> {
+        val currentBirthdays = ArrayList<Birthday>()
+        database.birthdayList.forEach { n ->
+            if (n.month == calendar.get(Calendar.MONTH)+1 && n.day == calendar.get(Calendar.DAY_OF_MONTH) && n.daysToRemind == 0)
+                currentBirthdays.add(n)
         }
-        notificationManager.notify(100, builder.build())
+        return currentBirthdays
+    }
+
+    private fun notifyBirthdayNow(birthday: Birthday) {
+        createNotification("Birthdays",
+            "Birthday Notification",
+            100,
+            "Birthday",
+            "It's ${birthday.name}s birthday!"
+        )
+    }
+
+    private fun notifyCurrentBirthdays(currentBirthdays: Int) {
+        createNotification("Birthdays",
+            "Birthday Notification",
+            102,
+            "Birthdays",
+            "There are $currentBirthdays birthdays today!"
+        )
     }
 
     private fun notifyUpcomingBirthday(birthday: Birthday) {
+        createNotification("Upcoming Birthdays",
+            "Birthday Notification",
+            101,
+            "Upcoming Birthday",
+            "${birthday.name}s birthday is coming up in ${birthday.daysToRemind} days!"
+        )
+    }
+
+    private fun notifyUpcomingBirthdays(upcomingBirthdays: Int) {
+        createNotification("Upcoming Birthdays",
+            "Birthday Notification",
+            103,
+            "Upcoming Birthdays",
+            "$upcomingBirthdays birthdays are coming up!"
+        )
+    }
+
+    private fun createNotification(channelId: String, name: String, requestCode: Int, contentTitle: String, contentText: String) {
         val notificationManager = myContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         val repeatingIntent = Intent(myContext, MainActivity::class.java)
 
         repeatingIntent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
 
-        val pendingIntent = PendingIntent.getActivity(myContext, 100, repeatingIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+        val pendingIntent = PendingIntent.getActivity(myContext, requestCode, repeatingIntent, PendingIntent.FLAG_UPDATE_CURRENT)
 
-        val channelId = "Birthdays"
         val builder: Notification.Builder
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val notificationChannel = NotificationChannel(channelId, "Birthday Notifications", NotificationManager.IMPORTANCE_HIGH)
+            val notificationChannel = NotificationChannel(channelId, name, NotificationManager.IMPORTANCE_HIGH)
             notificationChannel.enableLights(true)
             notificationChannel.lightColor = Color.GREEN
             notificationChannel.enableVibration(true)
             notificationManager.createNotificationChannel(notificationChannel)
 
             builder = Notification.Builder(myContext, channelId)
-                .setContentTitle("Birthday")
-                .setContentText("${birthday.name}s birthday is coming up in ${birthday.daysToRemind} days!")
+                .setContentTitle(contentTitle)
+                .setContentText(contentText)
                 .setSmallIcon(R.drawable.ic_action_birthday)
                 .setLargeIcon(
                     BitmapFactory.decodeResource(myContext.resources,
@@ -133,8 +148,8 @@ class NotificationReceiver : BroadcastReceiver() {
                 .setAutoCancel(true)
         } else {
             builder = Notification.Builder(myContext)
-                .setContentTitle("Birthday")
-                .setContentText("${birthday.name}s birthday is coming up in ${birthday.daysToRemind} days!")
+                .setContentTitle(contentTitle)
+                .setContentText(contentText)
                 .setSmallIcon(R.drawable.ic_action_birthday)
                 .setLargeIcon(
                     BitmapFactory.decodeResource(myContext.resources,
@@ -143,6 +158,6 @@ class NotificationReceiver : BroadcastReceiver() {
                 .setContentIntent(pendingIntent)
                 .setAutoCancel(true)
         }
-        notificationManager.notify(100, builder.build())
+        notificationManager.notify(requestCode, builder.build())
     }
 }
