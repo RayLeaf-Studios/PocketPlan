1a0fe26debc0f570a8dbb596716e045363560d9a
Eugen Stepanov <e.stepanov611@gmail.com>
2020-04-13 02:35:42 +0200

    redone SleepReminder, class uses now a companion object for interactions

--- app/src/main/java/com/example/j7_003/data/SleepReminder.kt
+++ app/src/main/java/com/example/j7_003/data/SleepReminder.kt
@@ -1,31 +1,77 @@
 package com.example.j7_003.data
 
-import android.util.Log
+import android.app.*
+import android.content.Context
+import android.content.Intent
+import com.example.j7_003.MainActivity
+import com.example.j7_003.notifications.NotificationReceiver
 import java.util.*
 import kotlin.properties.Delegates
 
-class SleepReminder(var reminderHour: Int, var reminderMinute: Int) {
-    private val myCalendar = Calendar.getInstance()
-    private var currentHour by Delegates.notNull<Int>()
-    private var currentMinute by Delegates.notNull<Int>()
+class SleepReminder {
 
-    init {
-        getClock()
-        if(isRemindTimeReached()) {
-            Log.e("True", "${reminderHour.compareTo(currentHour)} ${reminderMinute.compareTo(currentMinute)}")
-        } else {
-            Log.e("False", "${reminderHour.compareTo(currentHour)} ${reminderMinute.compareTo(currentMinute)}")
+    companion object {
+        private val myCalendar = Calendar.getInstance()
+        private var currentHour by Delegates.notNull<Int>()
+        private var currentMinute by Delegates.notNull<Int>()
+
+        var reminderHour: Int = 0
+        var reminderMinute: Int = 0
+
+        var isSet: Boolean = false
+
+        fun isRemindTimeReached(): Boolean {
+            getClock()
+
+            return (compareHours() == -1) || (compareHours() == 0 && compareMinutes() < 1)
         }
-    }
 
+        fun edit(newHour: Int, newMinute: Int) {
+            reminderHour = newHour
+            reminderMinute = newMinute
+        }
 
-    private fun isRemindTimeReached(): Boolean = (compareHours() == -1) || (compareHours() == 0 && compareMinutes() < 1)/*|| (compareHours() == 1 && compareMinutes() < 0)*/
+        fun disable() {
+            isSet = false
+        }
 
-    private fun compareHours(): Int = reminderHour.compareTo(currentHour)
-    private fun compareMinutes(): Int = reminderMinute.compareTo(currentMinute)
+        fun enable() {
+            isSet = true
 
-    private fun getClock() {
-        currentHour = myCalendar.get(Calendar.HOUR_OF_DAY)
-        currentMinute = myCalendar.get(Calendar.MINUTE)
+        }
+
+        private fun compareHours(): Int = reminderHour.compareTo(currentHour)
+        private fun compareMinutes(): Int = reminderMinute.compareTo(currentMinute)
+
+        private fun getClock() {
+            currentHour = myCalendar.get(Calendar.HOUR_OF_DAY)
+            currentMinute = myCalendar.get(Calendar.MINUTE)
+        }
+
+        fun setSleepReminderAlarm() {
+            if (isSet) {
+                val intent = Intent(MainActivity.myActivity, NotificationReceiver::class.java)
+                val pendingIntent = PendingIntent.getBroadcast(
+                    MainActivity.myActivity,
+                    200,
+                    intent,
+                    PendingIntent.FLAG_UPDATE_CURRENT
+                )
+                val alarmManager =
+                    MainActivity.myActivity.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+
+                val notificationTime = Calendar.getInstance()
+                notificationTime.set(Calendar.HOUR_OF_DAY, reminderHour)
+                notificationTime.set(Calendar.MINUTE, reminderHour)
+                notificationTime.set(Calendar.SECOND, 0)
+
+                alarmManager.setRepeating(
+                    AlarmManager.RTC_WAKEUP,
+                    notificationTime.timeInMillis,
+                    AlarmManager.INTERVAL_DAY,
+                    pendingIntent
+                )
+            }
+        }
     }
 }
