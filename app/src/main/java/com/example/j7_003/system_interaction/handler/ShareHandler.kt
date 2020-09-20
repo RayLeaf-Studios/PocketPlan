dddc39595cdb26b6847c3f6ec1740c72eacff26a
Eugen Stepanov <e.stepanov611@gmail.com>
2020-09-20 23:57:45 +0200

    single files can now be shared

--- app/src/main/java/com/example/j7_003/system_interaction/handler/ShareHandler.kt
+++ app/src/main/java/com/example/j7_003/system_interaction/handler/ShareHandler.kt
@@ -2,6 +2,7 @@ package com.example.j7_003.system_interaction.handler
 
 import android.content.Intent
 import android.net.Uri
+import android.widget.Toast
 import androidx.core.content.FileProvider
 import com.example.j7_003.MainActivity
 
@@ -18,6 +19,17 @@ class ShareHandler: FileProvider() {
         sharingIntent.type = "text/*"
 
         sharingIntent.putExtra(Intent.EXTRA_STREAM, list)
+
+        MainActivity.act.startActivity(Intent.createChooser(sharingIntent, "Share via"))
+    }
+
+    fun shareById(id: StorageId) {
+        val uri = getUriForFile(MainActivity.act,
+            "${MainActivity.act.applicationContext.packageName}.provider", StorageHandler.files[id]!!)
+        val sharingIntent = Intent(Intent.ACTION_SEND)
+        sharingIntent.type = "text/*"
+
+        sharingIntent.putExtra(Intent.EXTRA_STREAM, uri)
 
         MainActivity.act.startActivity(Intent.createChooser(sharingIntent, "Share via"))
     }
