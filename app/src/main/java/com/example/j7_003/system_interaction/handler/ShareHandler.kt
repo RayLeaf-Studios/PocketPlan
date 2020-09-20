b5a7284e127bd1dfb7edb0c2be940abcb81d11dd
Eugen Stepanov <e.stepanov611@gmail.com>
2020-09-20 23:10:24 +0200

    sharing all storage files is now possible

--- app/src/main/java/com/example/j7_003/system_interaction/handler/ShareHandler.kt
+++ app/src/main/java/com/example/j7_003/system_interaction/handler/ShareHandler.kt
@@ -2,19 +2,23 @@ package com.example.j7_003.system_interaction.handler
 
 import android.content.Intent
 import android.net.Uri
-import android.util.Log
 import androidx.core.content.FileProvider
 import com.example.j7_003.MainActivity
-import java.io.File
 
 class ShareHandler: FileProvider() {
     fun shareAll() {
-        val file: File = StorageHandler.files["SETTINGS"]!!
+        val list = ArrayList<Uri>()
+        StorageHandler.files.forEach { (_, file) ->
+            list.add(getUriForFile(
+                MainActivity.act, "${MainActivity.act.applicationContext.packageName}.provider",
+                file))
+        }
+
         val sharingIntent = Intent(Intent.ACTION_SEND_MULTIPLE)
         sharingIntent.type = "text/*"
-        sharingIntent.putExtra(Intent.EXTRA_STREAM, getUriForFile(MainActivity.act,
-            "${MainActivity.act.applicationContext.packageName}.provider", file))
-        sharingIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+
+        sharingIntent.putExtra(Intent.EXTRA_STREAM, list)
+
         MainActivity.act.startActivity(Intent.createChooser(sharingIntent, "Share via"))
     }
 }
