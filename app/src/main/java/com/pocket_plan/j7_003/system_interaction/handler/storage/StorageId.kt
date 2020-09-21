c4ce79ae79aec8f69af50a8600a1b3cd37c7ad32
Eugen Stepanov <e.stepanov611@gmail.com>
2020-09-21 17:51:00 +0200

    added zip id for upcoming backup feature

--- app/src/main/java/com/pocket_plan/j7_003/system_interaction/handler/storage/StorageId.kt
+++ app/src/main/java/com/pocket_plan/j7_003/system_interaction/handler/storage/StorageId.kt
@@ -1,5 +1,5 @@
 package com.pocket_plan.j7_003.system_interaction.handler.storage
 
 enum class StorageId {
-    SETTINGS, BIRTHDAYS, NOTES, SLEEP, SHOPPING, CALENDAR, WEEK, USER_TEMPLATE_LIST, TASKS
+    SETTINGS, BIRTHDAYS, NOTES, SLEEP, SHOPPING, CALENDAR, WEEK, USER_TEMPLATE_LIST, TASKS, ZIP
 }
