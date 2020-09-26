c15eb5f819f1ca2c72a35247b2b942fbeadff10c
Micha <micha.nowak98@gmx.de>
2020-09-26 19:48:54 +0200

    notes are searchable

--- app/src/main/java/com/pocket_plan/j7_003/data/notelist/NoteFr.kt
+++ app/src/main/java/com/pocket_plan/j7_003/data/notelist/NoteFr.kt
@@ -2,9 +2,8 @@ package com.pocket_plan.j7_003.data.notelist
 
 import android.os.Bundle
 import android.text.TextUtils
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.widget.SearchView
 import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat
@@ -13,12 +12,14 @@ import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.pocket_plan.j7_003.MainActivity
 import com.pocket_plan.j7_003.R
+import com.pocket_plan.j7_003.data.birthdaylist.BirthdayFr
 import com.pocket_plan.j7_003.data.fragmenttags.FT
 import com.pocket_plan.j7_003.data.notelist.NoteEditorFr.Companion.noteColor
 import com.pocket_plan.j7_003.data.settings.SettingId
 import com.pocket_plan.j7_003.data.settings.SettingsManager
 import kotlinx.android.synthetic.main.fragment_note.view.*
 import kotlinx.android.synthetic.main.row_note.view.*
+import java.util.*
 
 /**
  * A simple [Fragment] subclass.
@@ -26,11 +27,99 @@ import kotlinx.android.synthetic.main.row_note.view.*
 
 class NoteFr : Fragment() {
 
+    lateinit var myMenu: Menu
+    lateinit var searchView: SearchView
     companion object {
-        var deletedNote: Note? = null
-        lateinit var noteAdapter: NoteAdapter
+        lateinit var myAdapter: NoteAdapter
         var noteLines = 0
         val noteListInstance: NoteList = NoteList()
+        lateinit var myFragment: NoteFr
+
+        var searching = false
+        lateinit var adjustedList: ArrayList<Note>
+        lateinit var lastQuery: String
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_notes, menu)
+        myMenu = menu
+        adjustedList = arrayListOf()
+
+        searchView = menu.findItem(R.id.item_notes_search).actionView as SearchView
+        val textListener = object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                //todo fix this
+                //close keyboard?
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                if (searching) {
+                    myFragment.search(newText.toString())
+                }
+                return true
+            }
+        }
+
+        searchView.setOnQueryTextListener(textListener)
+
+        val onCloseListener = SearchView.OnCloseListener {
+            MainActivity.toolBar.title = getString(R.string.menuTitleNotes)
+            searchView.onActionViewCollapsed()
+            searching = false
+            myAdapter.notifyDataSetChanged()
+            true
+        }
+
+        searchView.setOnCloseListener(onCloseListener)
+
+        searchView.setOnSearchClickListener {
+            MainActivity.toolBar.title = ""
+            searching = true
+            adjustedList.clear()
+            myAdapter.notifyDataSetChanged()
+        }
+
+        updateNoteSearchIcon()
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    fun updateNoteSearchIcon(){
+        myMenu.findItem(R.id.item_notes_search).isVisible = NoteFr.noteListInstance.size > 0
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setHasOptionsMenu(true)
+        super.onCreate(savedInstanceState)
+    }
+
+    fun search(query: String) {
+        if (query == "") {
+            adjustedList.clear()
+        } else {
+            lastQuery = query
+            adjustedList.clear()
+            noteListInstance.forEach {note ->
+                if (note.content.toLowerCase(Locale.ROOT)
+                        .contains(query.toLowerCase(Locale.ROOT)) ||
+                    note.title.toLowerCase(Locale.ROOT)
+                        .contains(query.toLowerCase(Locale.ROOT))
+                ) {
+                    adjustedList.add(note)
+                }
+            }
+        }
+        myAdapter.notifyDataSetChanged()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.item_notes_search -> {
+                /* no-op, listeners for this view are implemented in onCreateOptionsMenu */
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onCreateView(
@@ -38,6 +127,7 @@ class NoteFr : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
+        myFragment = this
         //inflating layout for NoteFragment
         val myView = inflater.inflate(R.layout.fragment_note, container, false)
         initializeComponents(myView)
@@ -53,8 +143,8 @@ class NoteFr : Fragment() {
 
         //initialize Recyclerview and Adapter
         val myRecycler = myView.recycler_view_note
-        noteAdapter = NoteAdapter()
-        myRecycler.adapter = noteAdapter
+        myAdapter = NoteAdapter()
+        myRecycler.adapter = myAdapter
         val lm = StaggeredGridLayoutManager(noteColumns.toInt(), 1)
         myRecycler.layoutManager = lm
         myRecycler.setHasFixedSize(true)
@@ -65,7 +155,6 @@ class NoteFr : Fragment() {
 
 class NoteAdapter :
     RecyclerView.Adapter<NoteAdapter.NoteViewHolder>() {
-    private val noteList = NoteFr.noteListInstance
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
         val itemView = LayoutInflater.from(parent.context)
@@ -75,13 +164,24 @@ class NoteAdapter :
 
     override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
 
-        val currentNote = noteList.getNote(position)
+
+        val currentNote = when (NoteFr.searching) {
+            /**
+             * NoteFr is currently in search mode, current note gets grabbed from
+             * NoteFr. adjusted list
+             */
+            true -> NoteFr.adjustedList[position]
+            /**
+             * NoteFr is currently in normal mode, current note gets grabbed from noteList
+             */
+            false -> NoteFr.noteListInstance.getNote(position)
+        }
+
 
         //EDITING TASK VIA ONCLICK LISTENER ON RECYCLER ITEMS
-
         holder.itemView.setOnClickListener {
             MainActivity.editNoteHolder = currentNote
-            noteColor = noteList.getNote(holder.adapterPosition).color
+            noteColor = NoteFr.noteListInstance.getNote(holder.adapterPosition).color
             MainActivity.act.changeToFragment(FT.NOTE_EDITOR)
         }
 
@@ -91,7 +191,7 @@ class NoteAdapter :
         //replace following true condition with setting to display full note content
         holder.tvNoteContent.text = currentNote.content
 
-        //TODO replace the following two values with custom settings
+        //decide how many lines per note are shown, depending on teh setting noteLines
         if (NoteFr.noteLines == -1) {
             holder.tvNoteContent.maxLines = Int.MAX_VALUE
         } else {
@@ -99,23 +199,16 @@ class NoteAdapter :
             holder.tvNoteContent.ellipsize = TextUtils.TruncateAt.END
         }
 
-        val cardColor = when (currentNote.color) {
-            NoteColors.RED -> R.color.colorNoteRed
-            NoteColors.YELLOW -> R.color.colorNoteYellow
-            NoteColors.GREEN -> R.color.colorNoteGreen
-            NoteColors.BLUE -> R.color.colorNoteBlue
-            NoteColors.PURPLE -> R.color.colorNotePurple
-        }
-
-        holder.cvNoteCard.setCardBackgroundColor(
-            ContextCompat.getColor(
-                MainActivity.act,
-                cardColor
-            )
-        )
+        //set background color depending on currentNote.color
+        holder.cvNoteCard.setCardBackgroundColor(currentNote.color.resolved)
     }
 
-    override fun getItemCount() = noteList.size
+    override fun getItemCount(): Int {
+        return when (NoteFr.searching) {
+            true -> NoteFr.adjustedList.size
+            false -> NoteFr.noteListInstance.size
+        }
+    }
 
     //one instance of this class will contain one instance of row_task and meta data like position
     //also holds references to views inside the layout
