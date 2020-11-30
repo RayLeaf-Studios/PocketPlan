dd12e4478d5ad0ba9275e61d3490d140e16beb40
Micha <micha.nowak98@gmx.de>
2020-11-30 01:03:30 +0100

    layout and string content updates

--- app/src/main/java/com/pocket_plan/j7_003/data/settings/sub_categories/SettingsAboutFr.kt
+++ app/src/main/java/com/pocket_plan/j7_003/data/settings/sub_categories/SettingsAboutFr.kt
@@ -7,8 +7,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.pocket_plan.j7_003.MainActivity
 import com.pocket_plan.j7_003.R
 import kotlinx.android.synthetic.main.fragment_settings_about.view.*
+import kotlinx.android.synthetic.main.header_navigation_drawer.view.*
 
 class SettingsAboutFr : Fragment() {
 
@@ -22,6 +24,10 @@ class SettingsAboutFr : Fragment() {
             val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/estep248/Pocket-Plan"))
             startActivity(intent)
         }
+
+        //display current version name
+        val versionString = "v "+MainActivity.act.packageManager.getPackageInfo(MainActivity.act.packageName, 0).versionName
+        myView.tvVersionAbout.text = versionString
         return myView
     }
 
