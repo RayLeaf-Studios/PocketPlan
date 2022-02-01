8ac167ed91fda4da5d43523111a66e81458ee54f
JakeGonder <gonderjake@gmail.com>
2022-02-01 21:36:43 +0100

    Added new design for howto fragment, refactored SettingsHowTo.kt to SettingsHowToFr.kt

---
+++ app/src/main/java/com/pocket_plan/j7_003/data/settings/sub_categories/SettingsHowToFr.kt
@@ -0,0 +1,137 @@
+package com.pocket_plan.j7_003.data.settings.sub_categories
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.pocket_plan.j7_003.MainActivity
+import com.pocket_plan.j7_003.R
+import kotlinx.android.synthetic.main.fragment_settings_how_to.view.*
+import kotlinx.android.synthetic.main.row_howto.view.*
+import kotlinx.android.synthetic.main.row_howto.view.ivExpand
+import kotlinx.android.synthetic.main.row_howto_cat.view.*
+
+class HowToCategory(val nameId: Int, val iconId: Int, val elements: ArrayList<HowToElement>){}
+
+class HowToElement(val subNameId: Int, val explanationId: Int, var expanded: Boolean = false){}
+
+class SettingsHowTo : Fragment() {
+
+    lateinit var myAdapter: HowToAdapter
+    lateinit var myActivity: MainActivity
+
+
+    val howToList = arrayListOf(
+        HowToCategory(R.string.menuTitleTasks, R.drawable.ic_action_todo, arrayListOf(
+            HowToElement(R.string.edit_folder, R.string.howtoNotes),
+        )),
+    )
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        myActivity = activity as MainActivity
+        val myView = inflater.inflate(R.layout.fragment_settings_how_to, container, false)
+        val myRecycler = myView.recycler_view_howto
+
+        myAdapter = HowToAdapter(this, myActivity)
+        myRecycler.adapter = myAdapter
+
+        myRecycler.layoutManager = LinearLayoutManager(activity)
+        myRecycler.setHasFixedSize(true)
+
+        // Inflate the layout for this fragment
+        return myView
+    }
+}
+
+
+/**
+ * CATEGORY ADAPTER
+ */
+class HowToAdapter(private val myFragment: SettingsHowTo, val myActivity: MainActivity) :
+    RecyclerView.Adapter<HowToAdapter.HowToViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HowToViewHolder {
+        val itemView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.row_howto_cat, parent, false)
+        return HowToViewHolder(itemView)
+    }
+
+
+    @SuppressLint("InflateParams")
+    override fun onBindViewHolder(holder: HowToViewHolder, position: Int) {
+        val category = myFragment.howToList[position]
+
+        val subRecyclerView = holder.itemView.subRecyclerViewHowTo
+        val subAdapter = SubHowToAdapter(myFragment, category, myActivity)
+        subRecyclerView.layoutManager = LinearLayoutManager(myActivity)
+        subRecyclerView.adapter = subAdapter
+
+        holder.itemView.tvCategoryHowTo.text = myActivity.getString(category.nameId)
+        holder.itemView.ivHowToCategory.setImageResource(category.iconId)
+    }
+
+    override fun getItemCount() = myFragment.howToList.size
+
+    class HowToViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+}
+
+/**
+ * SUB ADAPTER
+ */
+class SubHowToAdapter(private val myFragment: SettingsHowTo, val category: HowToCategory, val myActivity: MainActivity) :
+    RecyclerView.Adapter<SubHowToAdapter.SubHowToViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SubHowToViewHolder {
+        val itemView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.row_howto, parent, false)
+        return SubHowToViewHolder(itemView)
+    }
+
+
+    @SuppressLint("InflateParams")
+    override fun onBindViewHolder(holder: SubHowToViewHolder, position: Int) {
+        //Get item and its contents
+        val item = category.elements[position]
+        val itemName = myActivity.getString(item.subNameId)
+        val itemExplanation = myActivity.getString(item.explanationId)
+        val itemExpanded = item.expanded
+
+        //set the contents to the text fields
+        holder.itemView.tvHowToSubTitle.text = itemName
+        holder.itemView.tvHowToExplanation.text = itemExplanation
+
+        //show howto element content, depending on expansion state
+        holder.itemView.tvHowToExplanation.visibility = when(itemExpanded){
+            true -> View.VISIBLE
+            else -> View.GONE
+        }
+
+        //Flip expansion arrow to show expansion state of category
+        holder.itemView.ivExpand.rotation = when (itemExpanded) {
+            true -> 180f
+            else -> 0f
+        }
+
+        //Onclick listener to expand howto element
+        holder.itemView.setOnClickListener {
+            category.elements[position].expanded++
+            myFragment.myAdapter.notifyItemChanged(myFragment.howToList.indexOf(category))
+        }
+    }
+
+    override fun getItemCount() = category.elements.size
+
+    class SubHowToViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+}
+
+//Override for boolean inc function, to flip it using ++
+operator fun Boolean.inc() = !this
+
